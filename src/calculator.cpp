39c8f3b61a916bd46dcefd1f405462aa876b0eee
Pupler <misha.reak45@gmail.com>
2025-11-22 22:02:34 +0100

    Feat: add continuous operation loop with user control

--- src/calculator.cpp
+++ src/calculator.cpp
@@ -26,58 +26,68 @@ namespace calculator {
     }
 
     bool run() {
-        double firstNumber, secondNumber;
-        char operation;
+        char continue_program = 'y';
 
-        cout << "====CALCULATOR====" << endl;
-        cout << "Enter first number: ";
-        cin >> firstNumber;
+        while (continue_program == 'y' || continue_program == 'Y') {
+            double firstNumber, secondNumber;
+            char operation;
 
-        if (cin.fail()) {
-            cout << "Error ocurred!" << endl;
-            cin.clear();
+            cout << "====CALCULATOR====" << endl;
+            cout << "Enter first number: ";
+            cin >> firstNumber;
+
+            if (cin.fail()) {
+                cout << "Error ocurred!" << endl;
+                cin.clear();
+                cin.ignore(1000, '\n');
+                return false;
+            }
+
+            cout << "Enter second number: ";
+            cin >> secondNumber;
+
+            if (cin.fail()) {
+                cout << "Error ocurred!" << endl;
+                cin.clear();
+                cin.ignore(1000, '\n');
+                return false;
+            }
+
+            cout << "Your numbers: " << firstNumber << " and " << secondNumber << endl;
+            cout << "Which operation would you like?(+, -, *, /): ";
+            cin >> operation;
+
+            switch (operation) {
+                double result;
+
+                case '+':
+                    result = add(firstNumber, secondNumber);
+                    cout << "Result: " << result << endl;
+                    break;
+                case '-':
+                    result = subtract(firstNumber, secondNumber);
+                    cout << "Result: " << result << endl;
+                    break;
+                case '*':
+                    result = multiply(firstNumber, secondNumber);
+                    cout << "Result: " << result << endl;
+                    break;
+                case '/':
+                    result = divide(firstNumber, secondNumber);
+                    cout << "Result: " << result << endl;
+                    break;
+                default:
+                    cout << "Error: Invalid operation!" << endl;
+                    break;
+            }
+
+            cout << "Continue program?(y/n): ";
+            cin >> continue_program;
+
             cin.ignore(1000, '\n');
-            return false;
         }
 
-        cout << "Enter second number: ";
-        cin >> secondNumber;
-
-        if (cin.fail()) {
-            cout << "Error ocurred!" << endl;
-            cin.clear();
-            cin.ignore(1000, '\n');
-            return false;
-        }
-
-        cout << "Your numbers: " << firstNumber << " and " << secondNumber << endl;
-        cout << "Which operation would you like?(+, -, *, /): ";
-        cin >> operation;
-
-        switch (operation) {
-            double result;
-
-            case '+':
-                result = add(firstNumber, secondNumber);
-                cout << "Result: " << result << endl;
-                break;
-            case '-':
-                result = subtract(firstNumber, secondNumber);
-                cout << "Result: " << result << endl;
-                break;
-            case '*':
-                result = multiply(firstNumber, secondNumber);
-                cout << "Result: " << result << endl;
-                break;
-            case '/':
-                result = divide(firstNumber, secondNumber);
-                cout << "Result: " << result << endl;
-                break;
-            default:
-                cout << "Error: Invalid operation!" << endl;
-                break;
-        }
-
+        cout << "Chao!" << endl;
         return true;
     }
 }
